c7933fd735152dc0b9beb7b717dc70a12d928c77
Alex Ford <a.sewall.ford@gmail.com>
2024-06-17 21:39:49 +0000

    Add iife build

--- scripts/bundle.mjs
+++ scripts/bundle.mjs
@@ -1,70 +1,93 @@
-import * as esbuild from 'esbuild';
-import * as fs from 'fs';
+import * as esbuild from "esbuild";
+import * as fs from "fs";
 
-const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+const pkg = JSON.parse(fs.readFileSync("package.json", "utf8"));
 
 const copyFileSync = (source, dest) => {
-  const content = fs.readFileSync(source, 'utf-8');
+  const content = fs.readFileSync(source, "utf-8");
   fs.writeFileSync(dest, content);
 };
 
 //const license = fs.readFileSync('LICENSE.md', 'utf8');
 const banner = {
-  js: `// glMatrix - v${pkg.version}`
-}
+  js: `// glMatrix - v${pkg.version}`,
+};
 
 const banner64 = {
-  js: `// glMatrix (Float64) - v${pkg.version}`
-}
+  js: `// glMatrix (Float64) - v${pkg.version}`,
+};
 
 // Standard release (Float32)
 
 // ES Modules build
-esbuild.build({
-  logLevel: 'info',
-  entryPoints: ['src/index.ts'],
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  banner,
-  format: 'esm',
-  outfile: 'dist/esm/gl-matrix.js',
-}).catch(() => process.exit(1));
+esbuild
+  .build({
+    logLevel: "info",
+    entryPoints: ["src/index.ts"],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    banner,
+    format: "esm",
+    outfile: "dist/esm/gl-matrix.js",
+  })
+  .catch(() => process.exit(1));
 
-// Common JS Build
-esbuild.build({
-  logLevel: 'info',
-  entryPoints: ['src/index.ts'],
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  banner,
-  format: 'cjs',
-  outfile: 'dist/cjs/gl-matrix.js',
-}).catch(() => process.exit(1));
+// CommonJS build
+esbuild
+  .build({
+    logLevel: "info",
+    entryPoints: ["src/index.ts"],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    banner,
+    format: "cjs",
+    outfile: "dist/cjs/gl-matrix.js",
+  })
+  .catch(() => process.exit(1));
+
+// IIFE build
+esbuild
+  .build({
+    logLevel: "info",
+    entryPoints: ["src/index.ts"],
+    bundle: true,
+    minify: false,
+    sourcemap: false,
+    banner,
+    globalName: "glMatrix4",
+    format: "iife",
+    outfile: "dist/iife/gl-matrix.js",
+  })
+  .catch(() => process.exit(1));
 
 // Float64 release
 
 // ES Modules build
-esbuild.build({
-  logLevel: 'info',
-  entryPoints: ['f64/src/index.ts'],
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  banner: banner64,
-  format: 'esm',
-  outfile: 'dist/esm/f64/gl-matrix.js',
-}).catch(() => process.exit(1));
+esbuild
+  .build({
+    logLevel: "info",
+    entryPoints: ["f64/src/index.ts"],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    banner: banner64,
+    format: "esm",
+    outfile: "dist/esm/f64/gl-matrix.js",
+  })
+  .catch(() => process.exit(1));
 
 // Common JS Build
-esbuild.build({
-  logLevel: 'info',
-  entryPoints: ['f64/src/index.ts'],
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  banner: banner64,
-  format: 'cjs',
-  outfile: 'dist/cjs/f64/gl-matrix.js',
-}).catch(() => process.exit(1));
+esbuild
+  .build({
+    logLevel: "info",
+    entryPoints: ["f64/src/index.ts"],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    banner: banner64,
+    format: "cjs",
+    outfile: "dist/cjs/f64/gl-matrix.js",
+  })
+  .catch(() => process.exit(1));
